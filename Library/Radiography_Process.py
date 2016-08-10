Bug fix to Radiography_Process so transmission calculation worked if no absorption coefficient was given and the input data were multidimensional.  Also allow for attenuation corrections in fluorescence processing to assume either colocation of absorption and fluorescence or that fluorescence is in the middle.

Library/Radiography_Process.py
@@ -6,6 +6,10 @@ April 23, 2014: Add the fcompute_extinction_lengths function.
 Edits
 May 5, 2014: Clean up and reorder fluorescence corrections.  Use masked arrays
             for the extinction correction calculations.
+August 10, 2016: Correct bug in fcompute_transmission so it works if 
+            clear_ratio == 0 and transmission is multidimensional
+August 10, 2016: Allow correction attenuation in fluorescence to be
+            either assuming colocation or fluorescence in the middle.
 '''
 import numpy as np
 from scipy.optimize import brentq
@@ -28,7 +32,7 @@ def fcompute_transmission(PIN, reference, clear_ratio = 0, PIN_dark=0, ref_dark 
     else:
         #Divide by the second to max value of converted PIN to get transmission
         #Use second-highest in case highest is an outlier
-        transmission = transmission/sorted(transmission)[-2 if len(transmission)>1 else -1]
+        transmission = transmission/sorted(transmission.flatten())[-2 if len(transmission.flatten())>1 else -1]
     return transmission
 
 def fcompute_extinction_lengths(PIN, reference, clear_ratio = 0, PIN_dark=0, ref_dark = 0,abs_value=True):
@@ -52,7 +56,8 @@ def fcompute_radiography_density(PIN, reference, clear_ratio = 0, PIN_dark=0, re
         return None
 
 def fcompute_fluorescence_fit_fast(raw_fluorescence, slow_events, fast_events, radiography, reference,
-                          integration_time = 1, abs_coeff = 0, baseline = False, time_const = 3.13e-7):
+                          integration_time = 1, abs_coeff = 0, baseline = False, time_const = 3.13e-7,
+                          colocated=True):
     '''Basic fluorescence data processing.  These are corrections that should
     stay the same for all fluorescence peaks.  They include, in order of correction:
     *Dead time.  This is detector-based, and should be first.  This is based
@@ -76,16 +81,13 @@ def fcompute_fluorescence_fit_fast(raw_fluorescence, slow_events, fast_events, r
     else:
         ext_lengths = -np.log(radiography)
     #
-    #Form correction for nonuniform intensity through sample.  
-    #This correction assumes that absorption and fluorescence are co-located
-    #along the beam path.
-    #Avoid points with very low absorption, correction will be nonexistent there.
-    intensity_correction = np.ones_like(ext_lengths)
-    is_sig = ext_lengths > 1e-5
-    intensity_correction[is_sig] = (1-np.exp(-ext_lengths[is_sig]))/ext_lengths[is_sig]
-    #
-    #Add this correction to the fluorescence
-    final_fluorescence /= intensity_correction
+    #Form correction for nonuniform intensity along beam path.  
+    #If we assume absorption and fluorescence are colocated, correct the data.
+    if colocated:
+        final_fluorescence /= fcorrect_incident_intensity_colocated(ext_lengths)
+    #Otherwise, if not colocated, assume fluorescence is in the middle.
+    else:
+        final_fluorescence /= fcorrect_incident_intensity_middle(ext_lengths)
     #
     #Subtract the minimum value from the raw_fluorescence to remove background
     if baseline:
@@ -124,7 +126,7 @@ def fdead_time_zero_function(actual_countrate,observed_countrate,time_constant):
     return actual_countrate * np.exp(-time_constant * actual_countrate) - observed_countrate
 
 def fcompute_fluorescence(raw_fluorescence, slow_events, fast_events, radiography, reference,
-                          abs_coeff = 0, baseline = False):
+                          abs_coeff = 0, baseline = False, colocated=True):
     '''Basic fluorescence data processing.  These are corrections that should
     stay the same for all fluorescence peaks.  
     They include, in order of correction:
@@ -143,12 +145,10 @@ def fcompute_fluorescence(raw_fluorescence, slow_events, fast_events, radiograph
     #Divide the fluorescence by live_time to correct.
     final_fluorescence = raw_fluorescence/live_time
     #
-    #Correct for the variations in the incoming intensity
-    final_fluorescence /= reference
+    #Correct for the variations in the incoming intensity.  Normalize the
+    #reference by its mean so we are still basically in detector counts.
+    final_fluorescence /= (reference / np.mean(reference))
     #
-    #Correction for attenuation of incident beam in the sample.
-    #This correction assumes that absorption and fluorescence are co-located
-    #along the beam path.
     #Convert radiography signal to extinction lengths.  If abs_coeff = 0, assume 
     #signal is transmission.
     if abs_coeff:
@@ -156,11 +156,13 @@ def fcompute_fluorescence(raw_fluorescence, slow_events, fast_events, radiograph
     else:
         ext_lengths = -np.log(radiography)
     #
-    #Form correction for nonuniform intensity through sample.  Avoid points with very low absorption
-    intensity_correction = fcorrect_incident_intensity_colocated(ext_lengths)
-    #
-    #Add this correction to the fluorescence
-    final_fluorescence /= intensity_correction
+    #Form correction for nonuniform intensity along beam path.  
+    #If we assume absorption and fluorescence are colocated, correct the data.
+    if colocated:
+        final_fluorescence /= fcorrect_incident_intensity_colocated(ext_lengths)
+    #Otherwise, if not colocated, assume fluorescence is in the middle.
+    else:
+        final_fluorescence /= fcorrect_incident_intensity_middle(ext_lengths)
     #
     #Subtract the minimum value from the raw_fluorescence to remove background
     if baseline:
@@ -177,3 +179,10 @@ def fcorrect_incident_intensity_colocated(ext_lengths):
     is_sig = ext_lengths > 1e-5
     intensity_correction[is_sig] = (1-np.exp(-ext_lengths[is_sig]))/ext_lengths[is_sig]
     return intensity_correction
+
+def fcorrect_incident_intensity_middle(ext_lengths):
+    '''Formulate correction to the fluorescence due to the attenuation of the 
+    incident beam in the sample.  Assume fluorescence is in the middle of the
+    absorption.
+    '''
+    return np.exp(-ext_lengths/2.0)