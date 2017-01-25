a8896ccfe480921817878dec489cc4a72575f4b3
akastengren <akastengren@anl.gov>
2017-01-25 12:05:08 -0600

    Remove several poorly written or obsolete files.

--- Library/Flip_through_traces.py
+++
@@ -1,6 +0,0 @@
-import ALK_Utilities as ALK
-file_path = ALK.fcorrect_path_start() + 'SprayData/Cycle_2014_2/Taitech/'
-file_nums = range(1319,1329)
-filename_list = ALK.fcreate_filename_list(file_nums, '7bmb1_', '.hdf5', 4, '', False)
-for filename in filename_list:
-    ALK.fplot_HDF_trace(file_path, filename, ['Radiography'], ['7bmb1:m26.VAL'])
