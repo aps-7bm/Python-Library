c03eedb8c510e1119c8ab12317e0a478e210fe11
Alan L Kastengren <akastengren@anl.gov>
2017-09-01 09:20:15 -0500

    Alterations to Combine_Scans so it works with 2D arrays.
    Also added logging.

--- Library/Combine_Scans.py
+++ Library/Combine_Scans.py
@@ -12,6 +12,11 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
+import logging
+
+#Set up logging
+logger = logging.getLogger('Combine_Scans')
+logger.addHandler(logging.NullHandler())
 
 def finitialize_new_hdf5(file_path,new_file_name):
     '''Initializes the new HDF5 file.
@@ -37,33 +42,40 @@ def fcombine_dataset(file_path,old_file_names,new_file,
     new_datset_name: name of dataset in combined file.
     ordering_name: name to be used to place data into datasets in ascending order.
     '''
-    print new_file
-    print new_dataset_name
+    logger.info("Writing dataset {0:s} to new dataset name {1:s}.".format(old_dataset_name,new_dataset_name))
+    print("Writing dataset {0:s} to new dataset name {1:s}.".format(old_dataset_name,new_dataset_name))
     #Make lists to hold the values of the dataset until we know the sizes.
     data_list = []
     ordering_name_list = []
     #Loop through the old files, finding the positioner arrays.
     for fname in old_file_names:
-        print fname
-        old_file = h5py.File(fname,'r')
-        data_list.append(retrieve_dataset_method(old_file,old_dataset_name))
-        #If we are ordering by a variable, make a list out of it.
-        if ordering_name:
-            ordering_name_list.append(np.mean(retrieve_dataset_method(old_file,ordering_name)))
+        print(fname)
+        with h5py.File(fname,'r') as old_file:
+            data_list.append(retrieve_dataset_method(old_file,old_dataset_name))
+            #If we are ordering by a variable, make a list out of it.
+            if ordering_name:
+                ordering_name_list.append(np.mean(retrieve_dataset_method(old_file,ordering_name)))
     #If an ordering_name is given, sort the data_list by these values
     if ordering_name:
-        dummy, data_list = zip(*sorted(zip(ordering_name_list, data_list)))
+#         print(ordering_name)
+#         print(ordering_name_list)
+#         print(data_list)
+        __, data_list = zip(*sorted(zip(ordering_name_list, data_list)))
     #Loop through these values, finding the max size from any individual data file
-    max_length = 0
+    max_length = data_list[0].shape
     for array in data_list:
-        if len(array) > max_length:
-            max_length = len(array)
-    print "Max length = " + str(max_length)
+        max_length = np.maximum(max_length,array.shape)
+    logger.info("Max length = " + str(max_length))
     #Initialize the dataset in the combined file
-    new_file.create_dataset(new_dataset_name,data=np.NAN*np.ones((max_length,len(data_list))))
+    dataset_size = list(np.append(max_length,len(data_list)))
+    logger.info("Final array size = " + str(dataset_size))
+    new_file.create_dataset(new_dataset_name,data=np.NAN*np.ones(dataset_size))
     #Fill the combined file
     for i,array in enumerate(data_list):
-        new_file[new_dataset_name][:len(array),i] = array
+        if len(array.shape) == 1:
+            new_file[new_dataset_name][:len(array),i] = array
+        elif len(array.shape) == 2:
+            new_file[new_dataset_name][:array.shape[0],:array.shape[1],i] = array
     return
 
 def ftop_level_dataset(hdf_file,dataset_name):
@@ -118,8 +130,6 @@ def fprocess_files(file_path,old_file_names,new_file_name,dataset_method_dict,na
     new_file = finitialize_new_hdf5(file_path,new_file_name)
     #Loop through the datasets in the dictionaries
     for old_dataset in dataset_method_dict.keys():
-        print old_dataset
-        print dataset_method_dict[old_dataset]
         fcombine_dataset(file_path,old_file_names,new_file,
                             dataset_method_dict[old_dataset],
                             old_dataset,
